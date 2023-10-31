b7eff759b5d34c886fb4b64e6bfdd0cabcd69c64
kawtarabd <biouikawtar@gmail.com>
2023-10-30 17:13:14 -0700

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Holberton School"
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str)
