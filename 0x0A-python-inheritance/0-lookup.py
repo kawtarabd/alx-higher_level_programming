62f147bda2df6ce93193a6983399045d96c6a69e
kawtarabd <biouikawtar@gmail.com>
2023-12-04 12:26:08 +0100

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""lookup method """
+
+
+def lookup(obj):
+    """lookup object attributes and methods.
+
+    :param obj:
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""MyList class."""
+
+
+class MyList(list):
+    """MyList class."""
+
+    def print_sorted(self):
+        """method for print_sorted."""
+        print(sorted(self))
