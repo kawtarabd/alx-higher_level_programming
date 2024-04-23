8e0cc97ea50c142d41dc7572f3c403153e1e1283
kawtarabd <biouikawtar@gmail.com>
2024-04-23 03:14:24 -0700

    update

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const squaresize = process.argv[2];
+const mysquare = parseInt(squareSize);
+const x = 'x';
+if (isNaN(mySquare)) {
+   console.log('Missing size');
+} else {
+for (let i = 0; i<mySquare; i++) {
+   console.log(x.repeat(mySquare));
+}
+}
